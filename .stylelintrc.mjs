154600bcaf30c175d0a1f7ea8063dbb46872b292
Mario Yepes <marioy47@gmail.com>
2024-06-22 17:13:51 -0500

    feat: stylelint installation and configuration

---
+++ .stylelintrc.mjs
@@ -0,0 +1,10 @@
+// .stylellintrc.mjs
+
+/** @type {import('stylelint').Config} */
+export default {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-astro",
+    "stylelint-prettier/recommended",
+  ],
+};
