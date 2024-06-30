6b6dc54d87dc1df6858dbf0257f518820d6346e4
Mario Yepes <marioy47@gmail.com>
2024-06-30 10:21:02 -0500

    feat: rss feed creation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from "astro/config";
+// astro.config.mjs
 
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://marioyepes.com/",
 	integrations: [tailwind()],
 });
