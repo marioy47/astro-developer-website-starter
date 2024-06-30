3476bc1cd4dbba4016a4c188a16f75c97936300b
Mario Yepes <marioy47@gmail.com>
2024-06-29 21:04:10 -0500

    feat: blog collection and index created

---
+++ src/content/config.ts
@@ -0,0 +1,31 @@
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+	type: "content",
+	schema: ({ image }) => {
+		return z.object({
+			title: z.string(),
+			date: z.coerce.date(),
+			update: z.coerce.date().optional(),
+			tags: z.array(z.string()),
+			cover: image().optional(),
+			draft: z.boolean().optional().default(false),
+		});
+	},
+});
+
+const projects = defineCollection({
+	type: "content",
+	schema: ({ image }) => {
+		return z.object({
+			title: z.string(),
+			cover: image(),
+			date: z.coerce.date(),
+			client: z.string(),
+			tags: z.array(z.string()),
+			summary: z.string(),
+		});
+	},
+});
+
+export const collections = { blog, projects };
