7cfcbb2b0bee8e829da61c2dcbc45adeb753853d
Mario Yepes <marioy47@gmail.com>
2024-07-01 20:43:18 -0500

    feat: pagefind installation configuration and use in index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // astro.config.mjs
 
 import tailwind from "@astrojs/tailwind";
+import pagefind from "astro-pagefind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://marioyepes.com/",
-	integrations: [tailwind()],
+	build: {
+		format: "directory",
+	},
+	integrations: [tailwind(), pagefind()],
 });
