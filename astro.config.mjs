e0600dc453d377fbad2311a22a5b51cb09129585
Mario Yepes <marioy47@gmail.com>
2024-07-06 21:49:56 -0500

    chore: enable light and dark mode in shiki code hightlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,22 @@ import tailwind from "@astrojs/tailwind";
 import pagefind from "astro-pagefind";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
+/*
+ * @link https://astro.build/config
+ */
 export default defineConfig({
 	site: "https://marioyepes.com/",
 	build: {
 		format: "directory",
 	},
 	integrations: [tailwind(), pagefind()],
+	markdown: {
+		shikiConfig: {
+			// List of themes here: https://shiki.style/themes
+			themes: {
+				light: "rose-pine-dawn",
+				dark: "rose-pine-moon",
+			},
+		},
+	},
 });
